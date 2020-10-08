8c1ce95f0ac971bb61813bdf27c5bdd60109c85f
Alan Chang <ace108@yahoo.com>
2020-10-08 10:28:44 +0800

    LENOVO

--- bat/update_global_covid.bat
+++ bat/update_global_covid.bat
@@ -6,6 +6,8 @@ ipython \projects\global-covid\notebooks\covid_by_countries.ipynb
 jupyter nbconvert \projects\global-covid\notebooks\covid_by_countries.ipynb --no-input --no-prompt --to html
 move /Y \projects\global-covid\notebooks\covid_by_countries.html \projects\global-covid\index.html 
 cd \projects\global_covid\
+REM
+echo 'updating github...'
 git add .
 git push
 git commit -m LENOVO
