13118159cb6505c422985d5180cdfb4461045cde
Alan Chang <ace108@yahoo.com>
2020-10-08 10:17:24 +0800

    LENOVO

--- bat/update_global_covid.bat
+++ bat/update_global_covid.bat
@@ -2,6 +2,7 @@ set mydate=%date:~10,4%%date:~7,2%%date:~4,2%
 echo %mydate%
 set ccomment=LENOVO-%mydate%
 echo %ccomment%
+ipython covid_by_countries.ipynb
 jupyter nbconvert \projects\global-covid\notebooks\covid_by_countries.ipynb --no-input --no-prompt --to html
 move /Y \projects\global-covid\notebooks\covid_by_countries.html \projects\global-covid\index.html 
 cd \projects\global_covid\
