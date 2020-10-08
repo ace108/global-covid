01121a4bc63fedfec77fb7c3f5f34fd770a2f6bd
Alan Chang <ace108@yahoo.com>
2020-10-08 16:36:33 +0800

    LENOVO-20201008

--- bat/update_global_covid.bat
+++ bat/update_global_covid.bat
@@ -10,7 +10,7 @@ REM
 echo 'updating github...'
 git add .
 git push
-git commit -m LENOVO
+git commit -m %ccomment%
 echo "open https://api.covid19api.com/summary"
 echo "open https://ace108.github.io/global-covid/"
 echo "open https://aitel.weebly.com/covid-19-by-countries.html"
