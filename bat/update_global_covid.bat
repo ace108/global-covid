522767c6ce1f3b4d4644d98d57308c5f001e6443
Alan Chang <ace108@yahoo.com>
2020-10-08 10:13:48 +0800

    LENOVO

--- bat/update_global_covid.bat
+++ bat/update_global_covid.bat
@@ -3,7 +3,7 @@ echo %mydate%
 set ccomment=LENOVO-%mydate%
 echo %ccomment%
 jupyter nbconvert \projects\global-covid\notebooks\covid_by_countries.ipynb --no-input --no-prompt --to html
-move notebooks\covid_by_countries.html \projects\global-covid\index.html /Y
+move /Y \projects\global-covid\notebooks\covid_by_countries.html \projects\global-covid\index.html 
 cd \projects\global_covid\
 git add .
 git push
