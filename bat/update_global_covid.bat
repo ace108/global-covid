beaba7e5316addddcf50eed7060acd8e530d6406
Alan Chang <ace108@yahoo.com>
2020-10-15 22:53:19 +0800

    LENOVO-20201015

--- bat/update_global_covid.bat
+++ bat/update_global_covid.bat
@@ -1,3 +1,6 @@
-ipython \projects\global-covid\notebooks\covid_by_countries.ipynb
+# ipython \projects\global-covid\notebooks\covid_by_countries.ipynb
 jupyter nbconvert \projects\global-covid\notebooks\covid_by_countries.ipynb --no-input --no-prompt --to html
 move /Y \projects\global-covid\notebooks\covid_by_countries.html \projects\global-covid\index.html 
+# ipython \projects\global-covid\notebooks\covid_by_countries.ipynb
+jupyter nbconvert \projects\global-covid\notebooks\covid_by_time_map.ipynb --no-input --no-prompt --to html
+move /Y \projects\global-covid\notebooks\covid_by_time_map.html \projects\global-covid\covid_time_map.html 
